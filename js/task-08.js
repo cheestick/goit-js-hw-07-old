7d6ab3e0e59183914c8c4287702dcca067274a3a
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2021-05-03 10:50:14 +0300

    destroy and box creation were added

---
+++ js/task-08.js
@@ -0,0 +1,32 @@
+const createButtonRef = document.querySelector(
+  `button[data-action = 'render']`,
+);
+const clearButtonRef = document.querySelector(
+  `button[data-action = 'destroy']`,
+);
+
+createButtonRef.addEventListener('click', createBoxes);
+clearButtonRef.addEventListener('click', destroyBoxes);
+
+const randomInt = max => Math.floor(Math.random() * max);
+const randomRGBcolor = () =>
+  `rgb(${randomInt(255)},${randomInt(255)},${randomInt(255)})`;
+
+function createBox(sideSize = 30) {
+  const box = document.createElement(`div`);
+  box.style.backgroundColor = randomRGBcolor();
+  box.style.width = `${sideSize}px`;
+  box.style.height = `${sideSize}px`;
+  return box;
+}
+
+function createBoxes(amount) {
+  //   for (let a = 0; a < amount; a += 1) {
+
+  //   }
+  document.querySelector(`#boxes`).append(createBox());
+}
+
+function destroyBoxes() {
+  document.querySelector(`#boxes`).innerHTML = ``;
+}
